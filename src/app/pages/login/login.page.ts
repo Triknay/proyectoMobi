9ecf9df3297f0ac7267252a2642d030e8574ebba
Triknay <ma.cavieresh@duocuc.cl>
2024-06-03 19:44:32 -0400

    Finalizado S1
    Sumativa 1 terminada

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, viewChild } from '@angular/core';
+import { AlertController, ToastController } from '@ionic/angular';
 
 
 @Component({
@@ -8,9 +9,42 @@ import { Component, OnInit, viewChild } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  usuario: string ="";
+  contrasena: string ="";
+
+
+  constructor(private alertController: AlertController, private toastController: ToastController) { }
 
   ngOnInit() {
+  }
+
+  revisar(){
+    //this.presentAlert(this.usuario)
+    this.presentToast('bottom', this.usuario)
+  }
+
+  async presentAlert(mensaje:string){
+    const alert = await this.alertController.create({
+      header: 'soy una confirmacion',
+      message: mensaje,
+      buttons: ['Aceptar']
+    });
+    await alert.present();
+  }
+
+  async presentToast(position: 'top' | 'middle' | 'bottom', mensaje:string) {
+    const toast = await this.toastController.create({
+      message: mensaje,
+      duration: 2000,
+      position: position,
+    });
+
+    await toast.present();
+  }
+
+  ValidarCredenciales(){
+    console.log(this.usuario);
+    console.log(this.contrasena);
   }
 
 }
